c39ef970d6b4bb58c493f909e35a0794177773b2
Mario Gutierrez <mario.gutierrez@blackboard.com>
2022-09-06 11:14:15 -0500

    Added integration test

---
+++ Innovecs.UnitTest/Controller/CompanyControllerTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Innovecs.API.Controllers;
+using Innovecs.API.Model.Request;
+using Innovecs.API.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Innovecs.API.Model.Response;
+
+namespace Innovecs.UnitTest.Controller
+{
+    public class CompanyControllerTest
+    {
+        private CompanyController companyController;
+        public Mock<ICompanyService> mockCompanyService = new Mock<ICompanyService>();
+
+        public CompanyControllerTest()
+        {
+            companyController = new CompanyController(mockCompanyService.Object);
+        }
+
+        [Fact]
+        public async Task GetAmount()
+        {
+            mockCompanyService.Setup(mock => mock.GetAmountAsync(It.IsAny<CompanyAmountRequest>())).ReturnsAsync(12);
+            var result = await companyController.GetAmount(new CompanyAmountRequest());  
+            Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, (result as OkObjectResult).StatusCode);
+        }
+
+        [Fact]
+        public async Task GetAmountNotFound()
+        {
+            mockCompanyService.Setup(mock => mock.GetAmountAsync(It.IsAny<CompanyAmountRequest>())).ThrowsAsync(new Exception());
+            var result = await companyController.GetAmount(new CompanyAmountRequest());
+           // var expected = new { Amount = 12 };
+            Assert.IsType<NoContentResult>(result);
+            Assert.Equal(204, (result as NoContentResult).StatusCode);
+        }
+
+        [Fact]
+        public async Task GetTotal()
+        {
+            mockCompanyService.Setup(mock => mock.GetTotalAsync(It.IsAny<CompanyTotalRequest>())).ReturnsAsync(12);
+            var result = await companyController.GetTotal(new CompanyTotalRequest());
+            Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, (result as OkObjectResult).StatusCode);
+        }
+
+        [Fact]
+        public async Task GetTotalNotFound()
+        {
+            mockCompanyService.Setup(mock => mock.GetTotalAsync(It.IsAny<CompanyTotalRequest>())).ThrowsAsync(new Exception());
+            var result = await companyController.GetTotal(new CompanyTotalRequest());
+            Assert.IsType<NoContentResult>(result);
+            Assert.Equal(204, (result as NoContentResult).StatusCode);
+        }
+
+        [Fact]
+        public async Task GetQuote()
+        {
+            mockCompanyService.Setup(mock => mock.GetQuoteAsync(It.IsAny<CompanyQuoteRequest>())).ReturnsAsync(12);
+            var result = await companyController.GetQuote(new CompanyQuoteRequest());
+            var expected = new QuoteResponse() { Quote = 12 };
+            Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, (result as OkObjectResult).StatusCode);
+            Assert.Equal(expected.Quote, ((result as OkObjectResult).Value as QuoteResponse).Quote);
+        }
+
+        [Fact]
+        public async Task GetQuoteNotFound()
+        {
+            mockCompanyService.Setup(mock => mock.GetQuoteAsync(It.IsAny<CompanyQuoteRequest>())).ThrowsAsync(new Exception());
+            var result = await companyController.GetQuote(new CompanyQuoteRequest());
+            Assert.IsType<NoContentResult>(result);
+            Assert.Equal(204, (result as NoContentResult).StatusCode);
+        }
+    }
+}
+
---
+++ Innoves.IntegrationTest/Controller/CompanyControllerIntegrationTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading.Tasks;
+using Innovecs.API.Controllers;
+using Innovecs.API.Model.Request;
+using Innovecs.API.Services;
+using Innovecs.API.Services.Contracts;
+using Innovecs.Domain.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Innoves.IntegrationTest.Controller
+{
+    public class CompanyControllerIntegrationTest
+    {
+        private CompanyController companyController;
+        private ICompanyService companyService;
+
+        public CompanyControllerIntegrationTest()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddTransient<ICompanyService, CompanyService>()
+                .BuildServiceProvider();
+            companyService = serviceProvider.GetService<ICompanyService>();
+            companyController = new CompanyController(companyService);
+        }
+
+        [Fact]
+        public async Task GetAmount()
+        {
+            var request = new CompanyAmountRequest()
+            {
+                Cartons = new string[] { "Carton1", "Carton2" },
+                Consignee = "Mario",
+                Consignor = "Alejandro"                
+            };
+            var result = await companyController.GetAmount(request);
+            Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, (result as OkObjectResult).StatusCode);
+        }
+
+        [Fact]
+        public async Task GetAmountNotFound()
+        {
+            var request = new CompanyAmountRequest()
+            {
+                Cartons = new string[] { "Carton5", "Carton6" },
+                Consignee = "Mario",
+                Consignor = "Alejandro"
+            };
+            var result = await companyController.GetAmount(request);
+            Assert.IsType<NoContentResult>(result);
+            Assert.Equal(204, (result as NoContentResult).StatusCode);
+        }
+
+        [Fact]
+        public async Task GetQuote()
+        {
+            var request = new CompanyQuoteRequest()
+            {
+                Destination = "Destination1",
+                Source = "SourceAddress1",
+                Packages = new System.Collections.Generic.List<string>
+                {
+                    "Package1", "Package3"
+                }
+            };
+            var result = await companyController.GetQuote(request);
+            Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, (result as OkObjectResult).StatusCode);
+        }
+
+        [Fact]
+        public async Task GetQuoteNotFound()
+        {
+            var request = new CompanyQuoteRequest()
+            {
+                Destination = "DestinationNotfound",
+                Source = "Source",
+                Packages = new System.Collections.Generic.List<string>
+                {
+                    "Package1", "Package3"
+                }
+            };
+            var result = await companyController.GetQuote(request);
+            Assert.IsType<NoContentResult>(result);
+            Assert.Equal(204, (result as NoContentResult).StatusCode);
+        }
+    }
+}
+
